30d1022010d205b2a45d0643a69ed56bd42c82ab
Christopher Lim <toffer.lim87@gmail.com>
2024-10-02 16:12:03 -0700

    - add health check

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const communityController = require('../controllers/communityController');
+const healthRoutes = require('./health');
 
 // Define the routes
 router.get('/communities', communityController.getAllCommunities);
 router.post('/communities', communityController.addCommunity);
 router.get('/communities/download', communityController.downloadCommunitiesCSV);
+router.use(healthRoutes);
 
 module.exports = router;
